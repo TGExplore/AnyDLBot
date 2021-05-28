b1f75decca1e7e722e697722faaed4f7e8908487
Naviya2 <74837844+Naviya2@users.noreply.github.com>
2021-05-28 23:11:31 +0530

    Update translations.py

--- translation.py
+++ translation.py
@@ -1,58 +1,59 @@
 class Translation(object):
-    START_TEXT = """Hello,
-This is a Telegram URL Upload Bot!
+    START_TEXT = """Hello 👋,
+You are warnly welcome to Leo URL Upload Bot🙂!
 
 <b>Please send me any direct download URL Link, i can upload to telegram as File/Video</b>
 
-/help for more details..
+Use /help for more details😶
 
-Support Group : @InFoTelGroup
+Made By : @naviya2 🇱🇰
+Support Group : @leosupportx 🇱🇰
 © @SpEcHlDe , @TGBotsZ & @CWProjects"""
-    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
-    ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
-    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+    RENAME_403_ERR = "සොරි ඔයාට අවසරයක් නැහැ මේ ෆයිල් එක Rename කරන්න😐."
+    ABS_TEXT = "කරුණාකර ආත්මාර්තකාමී වෙන්න එපා🤚"
+    UPGRADE_TEXT = "<b>👉 ඔයාගෙම clone බොට් එක සාදාගැනීමට අවශ්‍යනම්.. </b>  /help command එක භාවිතා කරන්න🙂"
+    FORMAT_SELECTION = "අපේක්ශිත ආකෘතිය තෝරන්න: <a href='{}'>file size might be approximate</a> \nඔයාට custom thumbnail එකක් යූස් කරන්න ඕන නම්,පහල බටන් ක්ලික් කිරීමට කලින් ඉක්මනට Thumbnail Photo එක එවන්න.\nඔයාට /deletethumbnail Command එක මගින් Auto Generate උන Thumbnail එක අයින් කරගන්න පුලුවන්😊."
+    SET_CUSTOM_USERNAME_PASSWORD = """ඔයාට premium videos Download කිර්‍රමට අවශ්‍යනම්,පහත ආකෘතියට අනුව ඇතුලත් කරන්න🙂:
 URL | filename | username | password"""
-    NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
-    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @TGBotsZ"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @TGBotsZ \nUploaded in {} seconds."
+    NOYES_URL = "@robot URL detected. කරුණාකර මෙම site එක භාවිතා කර https://shrtz.me/PtsVnf6 වේගවත් URL එකක් ලබාගෙන මට ලබාදෙන්න එවිට මම අනෙකුත් users ල සදහා slow down වීම වලක්වා ගත හැකිවේ😊."
+    DOWNLOAD_START = "Download කිරීමට උත්සහ දරමින් සිටී🙂"
+    UPLOAD_START = "දැන් අප්ලෝඩ් වෙමින් පවතී🙂"
+    RCHD_BOT_API_LIMIT = "උපරිම ධාරිතාව වන (50MB) ට වැඩි දේවල් අප්ලෝඩ් කිරීමට අපහසුය😐. එහෙත් අප්ලෝඩ් කිරීමට උත්සහ දරමින් පවතී🙂."
+    RCHD_TG_API_LIMIT = "මේ කාලය ඇතුලත  ඩවින්ලෝඩ් උනා😊{} seconds.\nDetected File Size: {}\nSorry ඒත් මට මේ ෆයිල් එක අප්ලෝඩ් කල නොහැක මක්නිසාදයත් Telegram හි අප්ලෝඩ් ලිමිට් එක 1.5gb වන නිසාය😐."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "මාව ප්‍රයෝජනවත් වෙනව කියල හිතෙනවනම් මේ චැනල් එකට ජොයින් වෙන්න අලුත් අප්ඩේට්ස් දැනගන්න🙂. Join : @new_ehi"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Download උනා {} seconds ඇතුලත😊. \nJoin : @TGBotsZ \nUpload උනා {} seconds ඇතුලත."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users ලට පුලුවන් වෙන්නෙ මේ ප්‍රමානය විතරයි: {}\nඑමනිසා ඔයාගෙ subscription එක /upgrade කරගන්න.\nමෙය අපගේ දෝශයක් යැයි ඔයාට හිතෙනවනම් අපේ support group එකෙහි පළ කරන්න<a href='https://telegram.dog/ThankTelegram'>@leosupportx</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail එක සේව් උනා මේ thumbnail එක මෙතැන් සිට videos /files සදහා යොදා ගැනේ🙂"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail එක delete කිරීම සාර්ථකයි🙂"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media clear කිරීම සාර්ථකයි🙂."
+    SAVED_RECVD_DOC_FILE = "Document එක Download කිරීම සාර්ථකයි🙂"
     CUSTOM_CAPTION_UL_FILE = " "
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    NO_CUSTOM_THUMB_NAIL_FOUND = "Custom Thumbnail එකක් නොමැත."
     NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    CURENT_PLAN_DETAILS = """Current plan details
+    CURENT_PLAN_DETAILS = """දැනට ඔයාට පවතින plan එක පිලිබද තොරතුරු😶
 --------
 Telegram ID: <code>{}</code>
 Plan name: Free Cloned User
-Expires on: 31/12/2020"""
-    HELP_USER = """Hai am URL Uploader bot..
+Expires on:Exp වෙන්නෙ නැතෝ😁"""
+    HELP_USER = """Hi i am Leo URL Uploader bot..
     
-1. Send url (Link|New Name with Extension).
-2. Send Custom Thumbnail (Optional).
+1. Send url (Link|New Name with Extension).(ලින්ක් එක යවන්න)
+2. Send Custom Thumbnail (Optional).(custom thumbnail එක යවන්න)
 3. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File with Screenshots
-   Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
+   SVideo - Give File as video with Screenshots(වීඩියෝ එකත් සමග screenshots ලබාගතහැක)
+   DFile  - Give File with Screenshots (ෆයිල් එක සමග screenshots ලබා ගත හැක)
+   Video  - Give File as video without Screenshots (වීඩියෝව පමනක් ලබගැනිමට)
+   DFile  - Give File without Screenshots (ෆයිල් එක පමණක් ලබාගැනිමට)
    
 <b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/QkAkSLBgoYw">Diploy</a>
 
 --------
 Send /me to know current plan details
 
-Support Group : @InFoTelGroup
-© @TGBotsZ"""
+Support Group : @leosupportx 🇱🇰
+© @new_ehi"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
